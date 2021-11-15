f8fc03e440c245c3507687c63c0936b1511d0cc9
Arash Ghalebi <aghalebi33@gmail.com>
2021-11-15 16:53:43 -0500

    Comments added to herd.py

--- herd.py
+++ herd.py
@@ -6,6 +6,7 @@ class Herd:
         self.dinosaurs = []
         #self.create_herd()
 
+    # Herd objects use the create_herd method to create three units for the Dinosaur team with a set name and attack power.
     def create_herd(self):
         dino1 = Dinosaur("Jainosaurus", 25)
         dino2 = Dinosaur("Spinosaurus", 25)
